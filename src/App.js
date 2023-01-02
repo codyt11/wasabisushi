167c2708aeff71c4a7bed3ca916574ec9ede922b
unknown <codyt11@hotmail.com>
2023-01-02 16:20:10 -0600

    carousel outline

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import Body from './components/Body';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <Header/>      
+      <Header/>
+      <Body/>      
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import React from "react";
 import './header.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -18,7 +17,7 @@ function Header () {
         <div><Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand></div>
         <Navbar.Toggle className="tog" aria-controls="basic-navbar-nav" />
         <Navbar.Collapse className="w-100 pt-3" id="basic-navbar-nav">
-          <Nav className="mx-auto w-75 d-flex justify-content-between">
+          <Nav className="nav-items mx-auto w-75 d-flex justify-content-between">
             <Nav.Link href="#home">HOME</Nav.Link>
             <Nav.Link href="#menu">MENU</Nav.Link>
             <Nav.Link href="#menu">ABOUT</Nav.Link>
